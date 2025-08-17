c94bca21aae77ebe7a7f9305bb0fe73d6b9f9b60
anil_akbay <anilakbay20@gmail.com>
2025-08-17 14:14:29 +0300

    Add project files.

---
+++ BlazorWebRtc_Domain/Common/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWebRtc_Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public BaseEntity()
+        {
+            CreateDate = DateTime.Now;
+            Id = Guid.NewGuid();
+        }
+        public DateTime CreateDate { get; set; }
+        public Guid Id { get; set; }
+    }
+}
