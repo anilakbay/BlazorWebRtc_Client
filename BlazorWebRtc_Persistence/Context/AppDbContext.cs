a71e12cc2891b825e617a9897b6b83c1cd003036
anil_akbay <anilakbay20@gmail.com>
2025-08-18 03:24:53 +0300

    OnModelCreation Metodunun Override Edilmesi ve Migration İşlemi yapıldı.
    Veritabanı bağlantısı için gerekli konfigürasyonlar yapılandırıldı!

--- BlazorWebRtc_Persistence/Context/AppDbContext.cs
+++ BlazorWebRtc_Persistence/Context/AppDbContext.cs
@@ -15,6 +15,31 @@ namespace BlazorWebRtc_Persistence.Context
         public DbSet<Request> Requests { get; set; }
         public DbSet<MessageRoom> MessageRooms { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Request>()
+              .HasOne(i => i.SenderUser)
+              .WithMany()
+              .HasForeignKey(i => i.SenderUserId)
+              .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Request>()
+             .HasOne(i => i.ReceiverUser)
+             .WithMany()
+             .HasForeignKey(i => i.ReceiverUserId)
+             .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<MessageRoom>()
+             .HasOne(i => i.SenderUser)
+             .WithMany()
+             .HasForeignKey(i => i.SenderUserId)
+             .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<MessageRoom>()
+             .HasOne(i => i.ReceiverUser)
+             .WithMany()
+             .HasForeignKey(i => i.ReceiverUserId)
+             .OnDelete(DeleteBehavior.Restrict);
+        }
+
 
 
     }
---
+++ BlazorWebRtc_Persistence/Migrations/20250818001851_init.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorWebRtc_Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserFriends",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserFriends", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordSalt = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProfilePicture = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MessageRooms",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SenderUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    ReceiverUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MessageRooms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MessageRooms_Users_ReceiverUserId",
+                        column: x => x.ReceiverUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_MessageRooms_Users_SenderUserId",
+                        column: x => x.SenderUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    SenderUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ReceiverUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Requests_Users_ReceiverUserId",
+                        column: x => x.ReceiverUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Requests_Users_SenderUserId",
+                        column: x => x.SenderUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserUserFriend",
+                columns: table => new
+                {
+                    FriendsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UsersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserUserFriend", x => new { x.FriendsId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_UserUserFriend_UserFriends_FriendsId",
+                        column: x => x.FriendsId,
+                        principalTable: "UserFriends",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserUserFriend_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MessageContent = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MessageRoomId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Messages_MessageRooms_MessageRoomId",
+                        column: x => x.MessageRoomId,
+                        principalTable: "MessageRooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Messages_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MessageRooms_ReceiverUserId",
+                table: "MessageRooms",
+                column: "ReceiverUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MessageRooms_SenderUserId",
+                table: "MessageRooms",
+                column: "SenderUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_MessageRoomId",
+                table: "Messages",
+                column: "MessageRoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_UserId",
+                table: "Messages",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_ReceiverUserId",
+                table: "Requests",
+                column: "ReceiverUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_SenderUserId",
+                table: "Requests",
+                column: "SenderUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserUserFriend_UsersId",
+                table: "UserUserFriend",
+                column: "UsersId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "Requests");
+
+            migrationBuilder.DropTable(
+                name: "UserUserFriend");
+
+            migrationBuilder.DropTable(
+                name: "MessageRooms");
+
+            migrationBuilder.DropTable(
+                name: "UserFriends");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
